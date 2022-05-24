b7142b13e6215c4ac929de35ff6e1a1195766d5d
Lucca Torrezilha Soares <luccatsoares@gmail.com>
2022-05-24 13:02:46 -0300

    refatoracao
    criacao de uma funcao responsavel por receber a direcao a ser andado e a ultima posicao parada(andar) que chama a funcao de desenhar a matriz passando os valores para ser adicionado o caracter 'o' no lugar correto

--- joguinho.py
+++ joguinho.py
@@ -3,10 +3,24 @@ import os
 
 PAREDES = "*"
 
-NUM_LINHA = 5
-NUM_COLUNA = 5
+NUM_LINHA = 10
+NUM_COLUNA = 10
+
+
+#calcular meio da matriz
+if NUM_LINHA%2 != 0:
+    meio_linha = (NUM_LINHA + 1)/2
+else:
+    meio_linha = NUM_LINHA / 2
+
+if NUM_COLUNA%2 != 0:
+    meio_coluna = (NUM_COLUNA + 1)/2
+else:
+    meio_coluna = NUM_COLUNA/2
 
 matriz = []
+
+
 
 def criaMatriz(direcao, posicao_i, posicao_j):
     ultima_posicao_i = posicao_i
@@ -26,15 +40,7 @@ def criaMatriz(direcao, posicao_i, posicao_j):
                 elif j == NUM_COLUNA-1:
                     linha.append(PAREDES)
                 else:
-                    if direcao == '' and i == 2 and j == 2:
-                        linha.append('o')
-                        ultima_posicao_i = i
-                        ultima_posicao_j = j
-                    elif direcao == 'w' and i == ultima_posicao_i - 1 and j == ultima_posicao_j:
-                        linha.append('o')
-                        ultima_posicao_i = i
-                        ultima_posicao_j = j
-                    elif direcao == 's' and i == ultima_posicao_i and j == ultima_posicao_j -1:
+                    if direcao != "" and i == ultima_posicao_i and j == ultima_posicao_j:
                         linha.append('o')
                         ultima_posicao_i = i
                         ultima_posicao_j = j
@@ -43,6 +49,7 @@ def criaMatriz(direcao, posicao_i, posicao_j):
         matriz.append(linha)
         posicoes[0] = ultima_posicao_i
         posicoes[1] = ultima_posicao_j
+        
     return posicoes
 
 def desenhaMatriz():
@@ -51,34 +58,69 @@ def desenhaMatriz():
     return matriz
 
 
-direcao = ''
-retorno = criaMatriz(direcao,2,2)
-desenhaMatriz()
-print(f"{retorno}")
+def andar(direcao, posicao_i, posicao_j):
+    retorno = 0
 
-while(True):
+    if direcao == "i":
+        ultima_posicao_i = posicao_i
+        ultima_posicao_j = posicao_j
+        retorno = criaMatriz(direcao, ultima_posicao_i, ultima_posicao_j)
+        desenhaMatriz()
+        matriz.clear()
+
+    if direcao == "w":
+        ultima_posicao_i = posicao_i - 1
+        ultima_posicao_j = posicao_j
+        retorno = criaMatriz(direcao, ultima_posicao_i, ultima_posicao_j)
+        desenhaMatriz()
+        matriz.clear()
+
+    if direcao == "s":
+        ultima_posicao_i = posicao_i + 1
+        ultima_posicao_j = posicao_j
+        retorno = criaMatriz(direcao, ultima_posicao_i, ultima_posicao_j)
+        desenhaMatriz()
+        matriz.clear()
+    
+    if direcao == "a":
+        ultima_posicao_i = posicao_i
+        ultima_posicao_j = posicao_j - 1
+        retorno = criaMatriz(direcao, ultima_posicao_i, ultima_posicao_j)
+        desenhaMatriz()
+        matriz.clear()
+
+    if direcao == "d":
+        ultima_posicao_i = posicao_i
+        ultima_posicao_j = posicao_j + 1
+        retorno = criaMatriz(direcao, ultima_posicao_i, ultima_posicao_j)
+        desenhaMatriz()
+        matriz.clear()
+    return retorno
     
 
+print("INICIO\n")
+retorno = andar("i",meio_coluna,meio_linha)
+print(retorno)
 
-    if keyboard.is_pressed('w'):
-        direcao = 'w'
-        retorno = criaMatriz(direcao,retorno[0], retorno[1])
-        desenhaMatriz()
-        print(retorno)
-    if keyboard.is_pressed('s'):
-        direcao = 's'
-        criaMatriz(direcao,retorno[0], retorno[1])
-        desenhaMatriz()
-        print(retorno)
-    if keyboard.is_pressed('d'):
-        direcao = 'd'
-        criaMatriz(direcao,retorno[0], retorno[1])
-        desenhaMatriz()
-        print(retorno)
-    if keyboard.is_pressed('a'):
-        direcao = 'a'
-        criaMatriz(direcao,retorno[0], retorno[1])
-        desenhaMatriz()
-        print(retorno)
+print("ANDEI PRA CIMA\n")
+retorno = andar("w",retorno[0],retorno[1])
+print(retorno)
+print("ANDEI PRA BAIXO\n")
+retorno = andar("s",retorno[0],retorno[1])
+print(retorno)
 
+print("ANDEI PRA ESQUERDA\n")
+retorno = andar("a",retorno[0],retorno[1])
+print(retorno)
 
+print("ANDEI PRA DIREITA\n")
+retorno = andar("d",retorno[0],retorno[1])
+print(retorno)
+    
+
+'''
+if keyboard.is_pressed('w'):
+    direcao = 'w'
+    retorno = andar(direcao,retorno[0], retorno[1])
+    print(retorno)
+'''
